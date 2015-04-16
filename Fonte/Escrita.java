1ea0ddbe72a044d342b734d1b0ac85ddeb96ac1a
AndreHofer <andreluizhofer@gmail.com>
2015-04-16 00:24:59 -0300

    Declaração de variáveis

---
+++ Fonte/Escrita.java
@@ -0,0 +1,11 @@
+class Escrita extends Variavel{
+	private String conteudo;
+	
+	public void setEscrita(double c){
+		this.conteudo = c;
+	}
+
+	public String getEscrita(){
+		return this.conteudo;
+	}	
+}
