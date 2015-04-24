22d46994490c548f961ce665da97a3f6b7e179a4
AndreHofer <andreluizhofer@gmail.com>
2015-04-24 17:00:53 -0300

    excluindo arquivos. class adicionando a classe inteiro

---
+++ Fonte/Inteiro.java
@@ -0,0 +1,12 @@
+class Inteiro extends Variavel{
+	private int valor;
+	
+	public void setInteiro(int v){
+		this.valor = v;
+	}
+
+	public int getInteiro(){
+		return this.valor;
+	}	
+
+}
--- Fonte/Variavel.java
+++ Fonte/Variavel.java
@@ -1,7 +1,5 @@
 class Variavel{
 	private String nome;
-	private double valor;
-	
 	
 	public void setNome(String n){
 		this.nome = n;
@@ -10,13 +8,7 @@ class Variavel{
 	public String getNome(){
 		return this.nome;
 	}
-
-	public void setValor(double n) {
-		this.valor = n;
+	public Variavel (){
+		this.nome="";
 	}
-	public double getValor() {
-		return this.valor;
-	}
-	
-
 }
