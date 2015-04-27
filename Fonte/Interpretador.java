e97d96791a8f1951357501086b149b311611ba7f
Marcelo Acordi <marcelopancotte@gmail.com>
2015-04-27 00:28:00 -0300

    Agora usando Polimorfismo

--- Fonte/Interpretador.java
+++ Fonte/Interpretador.java
@@ -25,184 +25,83 @@ class Interpretador {
    
     //Varrendo linha por linha, e chamando a função analisaLinha, para determinar do que se trata.
     public void tratamento(String l[]) {
-        this.linhas = l;;
+        this.linhas = l;
         for(int i = 0; (i < this.linhas.length) && (this.linhas[i] != null); i++) {
-                 
-                    String linhaAtual = this.linhas[i];
-                        
-                        analisaLinha(linhaAtual);
-                    
-                 
+                 String linhaAtual = this.linhas[i];
+                 analisaLinha(linhaAtual);
         }
-      /*  for(int y = 0;y < 2000 && atributos[y].getNome() != null; y++) {
-                System.out.println(y+" "+atributos[y].getNome() + " " + atributos[y].getValor());
+        //Imprimindo o conteúdo do vetor de variáveis
+       for(int y = 0;y < 2000 && atributos[y].getNome() != null; y++) {
+                if(atributos[y] instanceof Inteiro) {
+                    Inteiro n = (Inteiro) atributos[y];
+                    System.out.println(y+" "+n.getNome()+ " "+ n.getInteiro());   
+                }
+                
 
-            } */
+            } 
     }
 
-    //Esta  função determina se é uma declaração de variável, se é um loop, se é uma expressão aritmética.
-        public void analisaLinha(String l) {
+    
+    public void analisaLinha(String l) {
             String linhaAtual;
             linhaAtual = l;
-    
-    /*Início da parte que analisa se a linha se trata de uma declaração de variável*/ 
-    //Quebro a string em espaços em branco ex: Int valor = 1;
-    // linhaAtual[0] = "Int", linhaAtual[1] = "valor", linhaAtual[2] = "=", linhaAtual = "1"
-    //e então vou analisando ela
-
-            String[] linhaAtualPedacos = linhaAtual.split(" ");
-            //System.out.println(linhaAtualPedacos[0]);
-            int tamanho = linhaAtualPedacos.length - 1;
-                if((linhaAtualPedacos[0].equals("int")) ) {
-                    String ultimoElemento = linhaAtualPedacos[tamanho];
-                    tamanho = ultimoElemento.length();
-                    //System.out.println(ultimoElemento);
-                    //System.out.println(ultimoElemento.length());
-                    if(tamanho > 1) {
-                        if(ultimoElemento.charAt(tamanho - 1) == ';') {
-                            tratarDeVariavel(linhaAtual);
-                        }
-                    }else {
-                        if(ultimoElemento.equals(";")) {
-                            tratarDeVariavel(linhaAtual);
-                        }
-                    } 
-                }
-                    //String[] vetor_op = {'+','-','*','^','/','<','>','<=','>=','%'}
-                    //int cond = 0;
-                    String[] vetor_op = {"+","-","*","^","/","<",">","<=",">=","%","int"};
-                    boolean cond = false;
-                    
-                    for(int i = 0; i < vetor_op.length; i++) {
-                        String a = vetor_op[i];
-                        if(linhaAtual.indexOf(vetor_op[i])>= 0) {
-                            cond = true;
-                           
-                        }
-                    }
-                   
-                   if((linhaAtual.indexOf("=")>= 0) && (cond == false)) {
-                       String[] quebrandoTudo = linhaAtual.split("=");
-                            for(int r = 0; r < atributos.length && atributos[r].getNome() != null;r++) {
-                                    if(atributos[r].getNome().replaceAll(" ","").equals(quebrandoTudo[0].replaceAll(" ",""))) {
-                                        atributos[r].setValor(Double.parseDouble(quebrandoTudo[1].replaceAll(";","")));
-                                    }
-                              }
-
-
-                    }else if(linhaAtual.indexOf("=")>=0 && (cond == true)) {
-                        //System.out.println(linhaAtual);
-                        double a,b,result;
-                        String[] quebrandoTudo = linhaAtual.split("=");
-                        String ope = quebrandoTudo[1].replaceAll(" ","");
-                        //String[] variaveis_expressao = ope.split("+");
-                        if(quebrandoTudo[1].indexOf("+")>=0) {
-                          // System.out.println(ope.split("\+"));
-                        }
-                        
-
-                    }
-                
-
-                
-              
-    /*Fim da parte que analisa se a linha se trata de uma declaração de variável*/  
-            }
-
-
-            // Se já constatado se tratar da declaração de uma variável, eu inicio o tratamento para armazenar a mesma no vetor de variáveis
-        private void tratarDeVariavel(String l) {
-        String linha;
-        linha = l;
-        String[] linhaAtualPedacos = linha.split(" ");
-        //int tam = linhaAtualPedacos.length - 1;
-        String semTipo = ""; 
-        for (int i = 1; i < linhaAtualPedacos.length; i++) {
-            semTipo += linhaAtualPedacos[i];
-        }
-         if(semTipo.indexOf(",") >=0) {
-              String[] semTipoM = semTipo.split(",");
-              for(int u = 0; u < semTipoM.length; u++) {
-                   // System.out.println(semTipoM[u]);
-                    if(semTipoM[u].indexOf("=") >= 0) {
-                            String[] quebrandoTudo = semTipoM[u].split("=");
-                            double n = Double.parseDouble(quebrandoTudo[1].replaceAll(";",""));
-                            int i = 0;
-                              boolean status = false;
-                             while(status != true) {
-                                  if(atributos[i].getNome() == null && status != true) {
-                                    atributos[i].setNome(quebrandoTudo[0]);
-                                   atributos[i].setValor(n); 
-                                     status = true;
-                                     break;
-                                     }
-                                        i++;
-                                        }
-                                  }else{
-                                            
-                                        int y = 0;
-                                        boolean status = false;
-                                        while(status != true) {
-                                           if(atributos[y].getNome() == null && status != true) {
-                                                atributos[y].setNome(semTipoM[u].replaceAll(";",""));
-                                                atributos[y].setValor(0);
-                                                status = true;
-                                               break;
-                                         }
-                                         y++;
-                                     }
-                                }
-                    }
-
-         }else {
-		if(semTipo.indexOf("=") >=0) {
-			String[] separandoIgual = semTipo.split("=");
-            //System.out.println(separandoIgual[1]);
-            double  n =  Double.parseDouble(separandoIgual[1].replaceAll(";",""));
             
-            int i = 0;
-            boolean status = false;
-            while(status != true) {
-                if(atributos[i].getNome() == null && status != true) {
-                    atributos[i].setNome(separandoIgual[0]);
-                    atributos[i].setValor(n); 
-                     status = true;
-                    break;
-                }
-               i++;
-            }
-		}else{
-				//System.out.println(semTipo);
-                int y = 0;
-                boolean status = false;
-                while(status != true) {
-                    if(atributos[y].getNome() == null && status != true) {
-                        atributos[y].setNome(semTipo.replaceAll(";",""));
-                        atributos[y].setValor(0);
-                        status = true;
-                        break;
-                    }
-                    y++;
-                }
+            /* Aqui eu sei se a linha é uma declaração de variável                      */
+            /*                                                                          */
+            if((linhaAtual.indexOf("int") >= 0) || (linhaAtual.indexOf("double") >= 0)  || 
+                (linhaAtual.indexOf("string") >= 0)) {
+                if(l.indexOf(",") >= 0) {
+                    String[] vetorTamanho = l.split(",");
+                    int tamanhoVetor = vetorTamanho.length;
+                    Variavel[] vetor_var = new Variavel[tamanhoVetor];
 
+
+                }else {
+                  Variavel var = new Variavel();
+                  var = var.tratarDeclaracaoVariavel(linhaAtual);
+                  //System.out.println(var.getNome());
+                  this.inserirVariavel(var);
+                }
+            }
+            /*                                                                          */
+            /*Fim da parte que analisa se a linha se trata de uma declaração de variável*/ 
+
+
+            
+    }
+
+    public void inserirVariavel(Variavel var) {
+        int caminho = -1;
+        boolean status = false;
+        for(int i = 0;status == false; i++) {
+           // System.out.println(atributos[i].getNome());
+            if(atributos[i].getNome() == null && status == false) {
+                caminho = i;
+                status = true;
             }
         }
+        if(caminho != -1) {
+        if(var instanceof Inteiro) {
+            Inteiro n = (Inteiro) var;
+            atributos[caminho] = new Inteiro();
+            atributos[caminho] = n;
+            
 
+        }else if(var instanceof Escrita) {
+            Escrita n = (Escrita) var;
+            atributos[caminho] = new Escrita();
+            atributos[caminho] = n;
+        }else if(var instanceof Numeral) {
+            Numeral n = (Numeral) var;
+            atributos[caminho] = new Numeral();
+            atributos[caminho] = n;
+            }
+        }
     }
+
+
+
+    
 }
 
-
-  
-
-    
-    /*public void interpreta(String l[]) {
-        this.linhas = l;
-
-        for(int i = 0; i < this.linhas.length; i++) {
-            if(this.linhas[i] != null) {
-                // TODO: interpretar a linha
-                System.out.println("Linha " + (i + 1) + ": " + this.linhas[i]);
-            }
-        }
-    }*/
 
--- Fonte/Variavel.java
+++ Fonte/Variavel.java
@@ -1,6 +1,10 @@
 class Variavel{
 	private String nome;
 	
+	public Variavel (){
+		this.nome= null;
+	}
+	
 	public void setNome(String n){
 		this.nome = n;
 	}
@@ -8,7 +12,77 @@ class Variavel{
 	public String getNome(){
 		return this.nome;
 	}
-	public Variavel (){
-		this.nome="";
+
+	
+	/* Função tratarDeclaracaoVariavel nela eu quebro a linha e descubro os valores 
+		e retorno uma variável pronta com os dados */
+	public Variavel tratarDeclaracaoVariavel(String l) {
+		Variavel retornaVariavel = new Variavel();
+		String nome = "";
+		String linhaAtual = l;
+		String[] linhaQuebrada = null;
+		if(linhaAtual.indexOf("int") >= 0) {
+			retornaVariavel = new Inteiro();
+					linhaAtual = linhaAtual.replaceAll(" ","");
+					linhaAtual = linhaAtual.replaceAll("int","int ");
+					linhaQuebrada = linhaAtual.split(" ");
+		}else if(linhaAtual.indexOf("string") >= 0) {
+			retornaVariavel = new Escrita();
+					linhaAtual = linhaAtual.replaceAll(" ","");
+					linhaAtual = linhaAtual.replaceAll("string","string ");
+					 linhaQuebrada = linhaAtual.split(" ");
+		}else if(linhaAtual.indexOf("double") >= 0) {
+			retornaVariavel = new Numeral();
+					linhaAtual = linhaAtual.replaceAll(" ","");
+					linhaAtual = linhaAtual.replaceAll("double","double ");
+					linhaQuebrada = linhaAtual.split(" ");
+		}
+		String variavelDeclarada = linhaQuebrada[1];
+		if(!(linhaAtual.indexOf(",") >= 0)) {
+			if(!(linhaAtual.indexOf("=") >= 0)) {
+				if(variavelDeclarada.indexOf(";") >= 0) {
+					variavelDeclarada = variavelDeclarada.substring(0,variavelDeclarada.indexOf(";"));
+					retornaVariavel.setNome(variavelDeclarada);
+				}
+			}else{
+				if (retornaVariavel instanceof Inteiro) {
+					retornaVariavel = fazSet(variavelDeclarada,"inteiro");
+				}else if(retornaVariavel instanceof Numeral) {
+					retornaVariavel = fazSet(variavelDeclarada,"double");
+				}else if(retornaVariavel instanceof Escrita) {
+					retornaVariavel = fazSet(variavelDeclarada,"string");
+				}
+				
+			}
+		}
+			return retornaVariavel;
 	}
+
+	
+
+	private Variavel fazSet(String linha,String tipo) {
+		Variavel estadoAtual = new Variavel();
+		String[] nomeValor;
+		nomeValor = linha.split("=");
+		nomeValor[1] = nomeValor[1].replaceAll("[;\"]","");
+		if(tipo.equals("inteiro")) {
+			 Inteiro estado = new Inteiro();
+			estado.setNome(nomeValor[0]);
+			estado.setInteiro(Integer.valueOf(nomeValor[1]).intValue());
+			estadoAtual = estado;
+		}else if(tipo.equals("double")) {  
+			 Numeral estado = new Numeral();
+			estado.setNome(nomeValor[0]);
+			estado.setNumeral(Double.valueOf(nomeValor[1]).doubleValue());
+			estadoAtual = estado;
+		}else if(tipo.equals("string")) {
+			 Escrita estado = new Escrita();
+			estado.setNome(nomeValor[0]);
+			estado.setEscrita(nomeValor[1]);
+			estadoAtual = estado;
+		}
+		return estadoAtual;
+	}
+
+	
 }
